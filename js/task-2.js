4a0b7a665ded9256e0b76b23eafd6c389e85caaf
danielsydorenko <danielsydorenko1@gmail.com>
2025-06-02 22:40:39 +0200

    homework 4

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function calcAverageCalories(days) {
-  if (days.lenght === 0) {
+  if (days.length === 0) {
     return 0;
   }
   const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
-  return totalCalories / days.lenght;
+  return totalCalories / days.length;
 }
 
 console.log(
